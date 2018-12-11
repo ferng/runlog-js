7608c09420e40df06db9c62b0da6b5448e56c509
ferng001 <ferng001@gmail.com>
2018-12-11 08:27:07 +0000

    allow : in time entry

--- public/general/TimeEntry.jsx
+++ public/general/TimeEntry.jsx
@@ -22,6 +22,10 @@ const parseKeys = (key, pos, str) => {
     if (pos === 3 || pos === 6) {
       pos--;
     }
+  } else if (key === ':') {
+    if (pos === 2 || pos === 5) {
+      pos++;
+    }
   } else if (key === '0' || key === '1' || key === '2') {
     if (pos === 0 || pos === 1 || pos === 3 || pos === 4 || pos === 6 || pos === 7) {
       parsedTime = replaceAt(str, pos, key);
